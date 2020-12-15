5f09546c8b343492493d2e394fc16bd71835bd10
Victor Del Angel <Victor.Del@microsoft.com>
2020-12-15 19:19:02 +0000

    Updated AppIdentityDbContextSeed.cs

--- src/Infrastructure/Identity/AppIdentityDbContextSeed.cs
+++ src/Infrastructure/Identity/AppIdentityDbContextSeed.cs
@@ -8,6 +8,8 @@ namespace Microsoft.eShopWeb.Infrastructure.Identity
         public static async Task SeedAsync(UserManager<ApplicationUser> userManager)
         {
             var defaultUser = new ApplicationUser { UserName = "demouser@microsoft.com", Email = "demouser@microsoft.com" };
+            
+            //[SuppressMessage("Microsoft.Security", "CS002:SecretInNextLine", Justification="OHtest")]
             await userManager.CreateAsync(defaultUser, "Pass@word1");
         }
     }
